a24272fbf1930f1c5d11694da39aabab184a73cb
MoritzTheiel <theile@mtheile.com>
2021-02-25 12:03:11 +0100

    automated some more manual steps

--- utils/src/main/java/com/mtheile/utils/jhi/entityprocessor/EntityProcessor.java
+++ utils/src/main/java/com/mtheile/utils/jhi/entityprocessor/EntityProcessor.java
@@ -2,14 +2,52 @@ package com.mtheile.utils.jhi.entityprocessor;
 
 import java.io.File;
 
+import com.mtheile.utils.file.directory.FileUtils;
 import com.mtheile.utils.file.textfile.TextFileManipulator;
 
 public class EntityProcessor {
 
 	public static final String JHI_PROJ_HOME = "C:\\Users\\theil\\git\\com.lithodat.app";
+	public static final String DOMAIN_DIR = JHI_PROJ_HOME + "\\src\\main\\java\\com\\lithodat\\app\\domain\\";
+	public static final String SERVICE_DIR = JHI_PROJ_HOME + "\\src\\main\\java\\com\\lithodat\\app\\service";
+	public static final String MAPPER_DIR = JHI_PROJ_HOME + "\\src\\main\\java\\com\\lithodat\\app\\service\\mapper";
 
 	public static void main(String[] args) throws Exception {
+		makeLobFetchLazy();
+		addCascadingRemoves();
+		makeCreateSpecificationPublic();
+		fixUpperLowerCaseButInMapper();
+	}
 
+	private static void fixUpperLowerCaseButInMapper() throws Exception {
+		for (File file : FileUtils.listFiles(MAPPER_DIR, false)) {
+			if (!file.isDirectory()) {
+				TextFileManipulator.searchAndReplace("source = \"uPbSHRIMPSingleGrain", "source = \"UPbSHRIMPSingleGrain", file);
+				TextFileManipulator.searchAndReplace("target = \"uPbSHRIMPSingleGrain", "target = \"UPbSHRIMPSingleGrain", file);
+			}
+		}
+
+	}
+
+	private static void makeCreateSpecificationPublic() throws Exception {
+		for (File file : FileUtils.listFiles(SERVICE_DIR, false)) {
+			if (!file.isDirectory()) {
+				TextFileManipulator.searchAndReplace("private (.*) createSpecification", "public $1 createSpecification", file);
+			}
+		}
+
+	}
+
+	private static void makeLobFetchLazy() throws Exception {
+		for (File file : FileUtils.listFiles(DOMAIN_DIR, false)) {
+			if (!file.isDirectory()) {
+				TextFileManipulator.searchAndReplace("@Lob\\R", "@Lob @Basic(fetch = FetchType.LAZY)\n", file);
+			}
+		}
+
+	}
+
+	private static void addCascadingRemoves() throws Exception {
 		addCascadeTypeRemove("DataPackage.java", "dataPackage2Supervisors");
 		addCascadeTypeRemove("DataPackage.java", "dataPackage2Editors");
 		addCascadeTypeRemove("DataPackage.java", "dataPoints");
@@ -31,12 +69,11 @@ public class EntityProcessor {
 		addCascadeTypeRemove("Sample.java", "funding2Samples");
 		addCascadeTypeRemove("Sample.java", "analytes");
 		addCascadeTypeRemove("Sample.java", "images");
-
 	}
 
 	private static void addCascadeTypeRemove(String entityFile, String assoName) throws Exception {
 
-		File file = new File(JHI_PROJ_HOME + "\\src\\main\\java\\com\\lithodat\\app\\domain\\" + entityFile);
+		File file = new File(DOMAIN_DIR + entityFile);
 
 		TextFileManipulator.searchAndReplace(getRegex(assoName), replacement, file);
 
