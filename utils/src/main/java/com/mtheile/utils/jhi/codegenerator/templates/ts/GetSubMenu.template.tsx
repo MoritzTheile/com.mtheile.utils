9047b99baabeb8495595a0a7de50b16f234e9f47
MoritzTheile <theile@mtheile.com>
2023-04-11 13:06:59 +0200

    CodeGenerator: Menu WIP

---
+++ utils/src/main/java/com/mtheile/utils/jhi/codegenerator/templates/ts/GetSubMenu.template.tsx
@@ -0,0 +1,23 @@
+import { MenuItemLink } from 'react-admin';
+import resources from 'app/litho-ui/mydata/resources/resources';
+import TocIcon from '@material-ui/icons/Toc';
+import * as React from 'react';
+// import { entityLIrradiationReactorResourceName } from 'app/litho-ui/mydata/resources/l-irradiation-reactor/EntityResource';
+// <!-- CODEGENERATOR_NEEDLE_FOR_ADDING_IMPORTS (don't remove) -->
+
+export function getSubMenuMODELNAME_TOKEN(onMenuClick: () => void, open: boolean, dense: boolean) {
+  return (
+    <>
+      {/*<MenuItemLink
+        to={'/' + ftConfiguration.entityResourceName}
+        primaryText={`FT - FTDataPoint`}
+        leftIcon={<TocIcon />}
+        onClick={onMenuClick}
+        sidebarIsOpen={open}
+        dense={dense}
+      />*/}
+    {/*<!-- CODEGENERATOR_NEEDLE_FOR_ADDING_MENU_ENTRIES (don't remove) -->*/}
+
+    </>
+  );
+}
