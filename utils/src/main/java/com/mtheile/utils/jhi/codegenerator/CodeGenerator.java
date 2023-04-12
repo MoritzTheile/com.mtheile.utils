f0d83c4ebde89b186db512f49a1706ea441a39c2
MoritzTheile <theile@mtheile.com>
2023-04-12 12:28:10 +0200

    Code Generator can handle Lists in v1.0 now

--- utils/src/main/java/com/mtheile/utils/jhi/codegenerator/CodeGenerator.java
+++ utils/src/main/java/com/mtheile/utils/jhi/codegenerator/CodeGenerator.java
@@ -1,53 +1,92 @@
 package com.mtheile.utils.jhi.codegenerator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.mtheile.utils.file.textfile.TextFileManipulator;
 import com.mtheile.utils.jhi.codegenerator.AbstractTemplateProcessor.MODE;
 
 public class CodeGenerator {
 
 	private static final String PROJECT_HOME = "C:\\Users\\theil\\git\\com.lithodat.app\\";
-	private static final String modelName = "ICPMS";
-	//private static final String entityName = "LAblationCellType";
-	//private static final String entityName = "LCombinedMeasurement";
-	private static final String entityName = "LSampleIntroMethod";
-	
-	public static void main(String[] args) throws Exception {
-		
-//		generateListJavaCode();
-//		generateListJavaScriptCode();
-		generateListMenuEntries();
+
+	private static List<EntityMetaInfo> getEntityMetaInfos() {
+
+		List<EntityMetaInfo> entityMetaInfos = new ArrayList<>();
+
+		entityMetaInfos.add(new EntityMetaInfo("ICPMS", "LCombinedMeasurement"));
+		entityMetaInfos.add(new EntityMetaInfo("ICPMS", "LSampleIntroMethod"));
+		entityMetaInfos.add(new EntityMetaInfo("ICPMS", "LICPMSType"));
+		entityMetaInfos.add(new EntityMetaInfo("ICPMS", "LDataReductionPackage"));
+		entityMetaInfos.add(new EntityMetaInfo("ICPMS", "LSolutionIntroSystem"));
+		entityMetaInfos.add(new EntityMetaInfo("ICPMS", "LLaserSystem"));
+		entityMetaInfos.add(new EntityMetaInfo("ICPMS", "LLaserWaveLength"));
+		entityMetaInfos.add(new EntityMetaInfo("ICPMS", "LLaserPulseWidthUnit"));
+		entityMetaInfos.add(new EntityMetaInfo("ICPMS", "LLaserSamplingMode"));
+		entityMetaInfos.add(new EntityMetaInfo("ICPMS", "LAblationCellType"));
+		entityMetaInfos.add(new EntityMetaInfo("ICPMS", "LTubingMaterial"));
+		entityMetaInfos.add(new EntityMetaInfo("ICPMS", "LTubingMaterialInternalDiameter"));
+		entityMetaInfos.add(new EntityMetaInfo("ICPMS", "LInternalStandard"));
+		entityMetaInfos.add(new EntityMetaInfo("ICPMS", "LICPMSModel"));
+		entityMetaInfos.add(new EntityMetaInfo("ICPMS", "LZeroMCICPMS"));
+
+		return entityMetaInfos;
+
 	}
-	
-	private static void generateListMenuEntries() throws Exception{
-		
+
+	public static void main(String[] args) throws Exception {
+
+		for (EntityMetaInfo entityMetaInfo : getEntityMetaInfos()) {
+
+			generateListJavaCode(entityMetaInfo);
+			generateListJavaScriptCode(entityMetaInfo);
+			generateListMenuEntries(entityMetaInfo);
+
+		}
+	}
+
+	static class EntityMetaInfo {
+		public final String modelName;
+		public final String entityName;
+
+		public EntityMetaInfo(String modelName, String entityName) {
+			super();
+			this.modelName = modelName;
+			this.entityName = entityName;
+		}
+
+	}
+
+	private static void generateListMenuEntries(EntityMetaInfo entityMetaInfo) throws Exception {
+
 		// 01. Create SubMenu file if not exists
-		
+
 		new AbstractTemplateProcessor("ts/GetSubMenu.template.tsx") {
 
 			@Override
 			public String getTargetFilePath() {
 
-				return PROJECT_HOME + "src\\main\\webapp\\app\\litho-ui\\mydata\\layout\\submenues\\GetSubMenu"+modelName+".tsx";
+				return PROJECT_HOME + "src\\main\\webapp\\app\\litho-ui\\mydata\\layout\\submenues\\GetSubMenu" + entityMetaInfo.modelName + ".tsx";
 
 			}
 
 			@Override
 			public String processTemplate(String template) {
-				String result = template.replaceAll("ENTITYNAME_TOKEN", entityName);
-				result = result.replaceAll("MODELNAME_TOKEN", modelName);
+				String result = template.replaceAll("ENTITYNAME_TOKEN", entityMetaInfo.entityName);
+				result = result.replaceAll("MODELNAME_TOKEN", entityMetaInfo.modelName);
 				return result;
 			}
 
 		}.execute(MODE.SKIP_IF_FILE_EXISTS);
-		
+
 		// 02. Add SubMenu Code to main Menu if not already added.
-		
+
 		new AbstractTextFileProcessor(PROJECT_HOME + "src\\main\\webapp\\app\\litho-ui\\mydata\\layout\\Menu.tsx") {
 
 			@Override
 			public String processFileText(String text) throws Exception {
 				{ // adding import
-					String element = "import { getSubMenu"+modelName+" } from 'app/litho-ui/mydata/layout/submenues/GetSubMenu"+modelName+"';";
+					String element = "import { getSubMenu" + entityMetaInfo.modelName + " } from 'app/litho-ui/mydata/layout/submenues/GetSubMenu" + entityMetaInfo.modelName + "';";
 
 					if (!text.contains(element)) {
 						String replacement = //
@@ -58,42 +97,30 @@ public class CodeGenerator {
 					}
 				}
 				{ // adding resource
-					String element = "              <SubMenu\r\n"
-							+ "                handleToggle={() => handleToggle("+modelName+"')}\r\n"
-							+ "                isOpen={state."+modelName+"}\r\n"
-							+ "                sidebarIsOpen={open}\r\n"
-							+ "                isNested={true}\r\n"
-							+ "                name=\""+modelName+"\"\r\n"
-							+ "                icon={<ChevronRightIcon />}\r\n"
-							+ "                dense={dense}\r\n"
-							+ "                to={''}\r\n"
-							+ "              >\r\n"
-							+ "                {getSubMenu"+modelName+"(onMenuClick, open, dense)}\r\n"
-							+ "              </SubMenu>\r\n"
-							+ "";
+					String element = "              <SubMenu\r\n" + "                handleToggle={() => handleToggle('" + entityMetaInfo.modelName + "')}\r\n" + "                isOpen={state." + entityMetaInfo.modelName + "}\r\n" + "                sidebarIsOpen={open}\r\n" + "                isNested={true}\r\n" + "                name=\"" + entityMetaInfo.modelName + "\"\r\n" + "                icon={<ChevronRightIcon />}\r\n" + "                dense={dense}\r\n" + "                to={''}\r\n" + "              >\r\n" + "                {getSubMenu" + entityMetaInfo.modelName + "(onMenuClick, open, dense)}\r\n" + "              </SubMenu>\r\n" + "";
 
-					if (!text.contains(element)) {
+					if (!text.contains("isOpen={state." + entityMetaInfo.modelName + "}")) {
 						String replacement = //
 								element + "\n" + //
 										"        {/*<!-- CODEGENERATOR_NEEDLE_FOR_ADDING_MENU_ENTRIES (don't remove) -->*/}\n";
 
-						text = TextFileManipulator.replaceSection(text, "{/*", "CODEGENERATOR_NEEDLE_FOR_ADDING_RESOURCES", "*/}", replacement);
+						text = TextFileManipulator.replaceSection(text, "{/*", "CODEGENERATOR_NEEDLE_FOR_ADDING_MENU_ENTRIES", "*/}", replacement);
 					}
 				}
 				return text;
 			}
 		}.execute();
-		
+
 		// 03. Fill SubMenu file;
-		
-		new AbstractTextFileProcessor(PROJECT_HOME + "src\\main\\webapp\\app\\litho-ui\\mydata\\layout\\submenues\\GetSubMenu"+modelName+".tsx") {
+
+		new AbstractTextFileProcessor(PROJECT_HOME + "src\\main\\webapp\\app\\litho-ui\\mydata\\layout\\submenues\\GetSubMenu" + entityMetaInfo.modelName + ".tsx") {
 
 			@Override
 			public String processFileText(String text) throws Exception {
 				{ // adding import
-					String element = "import { getSubMenu"+modelName+" } from 'app/litho-ui/mydata/layout/submenues/GetSubMenu"+modelName+"';";
+					String element = "import { getSubMenu" + entityMetaInfo.modelName + " } from 'app/litho-ui/mydata/layout/submenues/GetSubMenu" + entityMetaInfo.modelName + "';";
 
-					if (!text.contains(element)) {
+					if (!text.contains("import { getSubMenu" + entityMetaInfo.modelName + " }")) {
 						String replacement = //
 								element + "\n" + //
 										"// <!-- CODEGENERATOR_NEEDLE_FOR_ADDING_IMPORTS (don't remove) -->\n";
@@ -102,15 +129,7 @@ public class CodeGenerator {
 					}
 				}
 				{ // adding resource
-					String element = ""
-							+ "<MenuItemLink\r\n"
-							+ "        to={'/"+modelName+"/"+entityName+"'}\r\n"
-							+ "        primaryText={`"+entityName+"`}\r\n"
-							+ "        leftIcon={<TocIcon />}\r\n"
-							+ "        onClick={onMenuClick}\r\n"
-							+ "        sidebarIsOpen={open}\r\n"
-							+ "        dense={dense}\r\n"
-							+ "      />";
+					String element = "" + "<MenuItemLink\r\n" + "        to={'/" + entityMetaInfo.modelName.toLowerCase() + "/" + entityMetaInfo.entityName + "'}\r\n" + "        primaryText={`" + entityMetaInfo.entityName + "`}\r\n" + "        leftIcon={<TocIcon />}\r\n" + "        onClick={onMenuClick}\r\n" + "        sidebarIsOpen={open}\r\n" + "        dense={dense}\r\n" + "      />";
 
 					if (!text.contains(element)) {
 						String replacement = //
@@ -125,8 +144,8 @@ public class CodeGenerator {
 		}.execute();
 	}
 
-	private static void generateListJavaCode() throws Exception{
-		
+	private static void generateListJavaCode(EntityMetaInfo entityMetaInfo) throws Exception {
+
 		// --------------- START - JAVA ----------------------------
 
 		new AbstractTemplateProcessor("ListLithoService.java.template") {
@@ -134,13 +153,13 @@ public class CodeGenerator {
 			@Override
 			public String getTargetFilePath() {
 
-				return PROJECT_HOME + "src\\main\\java\\com\\lithodat\\app\\litho\\service\\icpms\\" + entityName + "LithoService.java";
+				return PROJECT_HOME + "src\\main\\java\\com\\lithodat\\app\\litho\\service\\icpms\\" + entityMetaInfo.entityName + "LithoService.java";
 
 			}
 
 			@Override
 			public String processTemplate(String template) {
-				String result = template.replaceAll("ENTITYNAME_TOKEN", entityName);
+				String result = template.replaceAll("ENTITYNAME_TOKEN", entityMetaInfo.entityName);
 				return result;
 			}
 
@@ -151,14 +170,14 @@ public class CodeGenerator {
 			@Override
 			public String getTargetFilePath() {
 
-				return PROJECT_HOME + "src\\main\\java\\com\\lithodat\\app\\litho\\web\\rest\\"+modelName.toLowerCase()+"\\" + entityName + "LithoResource.java";
+				return PROJECT_HOME + "src\\main\\java\\com\\lithodat\\app\\litho\\web\\rest\\" + entityMetaInfo.modelName.toLowerCase() + "\\" + entityMetaInfo.entityName + "LithoResource.java";
 
 			}
 
 			@Override
 			public String processTemplate(String template) {
-				String result = template.replaceAll("ENTITYNAME_TOKEN", entityName);
-				result = result.replaceAll("MODELNAME_TOKEN", modelName.toLowerCase()); 
+				String result = template.replaceAll("ENTITYNAME_TOKEN", entityMetaInfo.entityName);
+				result = result.replaceAll("MODELNAME_TOKEN", entityMetaInfo.modelName.toLowerCase());
 				return result;
 			}
 
@@ -169,40 +188,41 @@ public class CodeGenerator {
 			@Override
 			public String getTargetFilePath() {
 
-				return PROJECT_HOME + "src\\main\\java\\com\\lithodat\\app\\litho\\service\\other\\batch\\tableimporter\\lists\\" + entityName + "Importer.java";
+				return PROJECT_HOME + "src\\main\\java\\com\\lithodat\\app\\litho\\service\\other\\batch\\tableimporter\\lists\\" + entityMetaInfo.entityName + "Importer.java";
 
 			}
 
 			@Override
 			public String processTemplate(String template) {
-				String result = template.replaceAll("ENTITYNAME_TOKEN", entityName);
-				result = result.replaceAll("MODELNAME_TOKEN", modelName.toLowerCase());
+				String result = template.replaceAll("ENTITYNAME_TOKEN", entityMetaInfo.entityName);
+				result = result.replaceAll("MODELNAME_TOKEN", entityMetaInfo.modelName.toLowerCase());
 				return result;
 			}
 
 		}.execute();
-		
+
 		new AbstractTextFileProcessor(PROJECT_HOME + "src\\main\\java\\com\\lithodat\\app\\litho\\service\\other\\batch\\dto\\BatchTableDTO.java") {
 
 			@Override
 			public String processFileText(String text) throws Exception {
-				
+
 				{ // adding resource
-					String element = ", " + entityName ;
-					
+					String element = ", " + entityMetaInfo.entityName;
+
 					if (!text.contains(element)) {
-						
+
 						String replacement = element + "\n" + "        // <!-- CODEGENERATOR_NEEDLE_FOR_ADDING_TABLES (don't remove) -->\n";
 
 						text = TextFileManipulator.replaceSection(text, "// <!--", "CODEGENERATOR_NEEDLE_FOR_ADDING_TABLES", "-->", replacement);
-						
+
 					}
 				}
 				return text;
 			}
 		}.execute();
 	}
-	private static void generateListJavaScriptCode() throws Exception{
+
+	private static void generateListJavaScriptCode(EntityMetaInfo entityMetaInfo) throws Exception {
 
 		// --------------- START - JAVASCRIPT ----------------------------
 		new AbstractTemplateProcessor("ts/EntityCreateFields.tsx") {
@@ -210,14 +230,14 @@ public class CodeGenerator {
 			@Override
 			public String getTargetFilePath() {
 
-				return PROJECT_HOME + "src\\main\\webapp\\app\\litho-ui\\mydata\\resources\\" + entityName + "\\EntityCreateFields.tsx";
+				return PROJECT_HOME + "src\\main\\webapp\\app\\litho-ui\\mydata\\resources\\"+entityMetaInfo.modelName+"\\" + entityMetaInfo.entityName + "\\EntityCreateFields.tsx";
 
 			}
 
 			@Override
 			public String processTemplate(String template) {
-				String result = template.replaceAll("ENTITYNAME_TOKEN", entityName);
-				result = result.replaceAll("MODELNAME_TOKEN", modelName.toLowerCase());
+				String result = template.replaceAll("ENTITYNAME_TOKEN", entityMetaInfo.entityName);
+				result = result.replaceAll("MODELNAME_TOKEN", entityMetaInfo.modelName);
 				return result;
 			}
 
@@ -227,14 +247,14 @@ public class CodeGenerator {
 			@Override
 			public String getTargetFilePath() {
 
-				return PROJECT_HOME + "src\\main\\webapp\\app\\litho-ui\\mydata\\resources\\" + entityName + "\\EntityCreateForm.tsx";
+				return PROJECT_HOME + "src\\main\\webapp\\app\\litho-ui\\mydata\\resources\\"+entityMetaInfo.modelName+"\\" + entityMetaInfo.entityName + "\\EntityCreateForm.tsx";
 
 			}
 
 			@Override
 			public String processTemplate(String template) {
-				String result = template.replaceAll("ENTITYNAME_TOKEN", entityName);
-				result = result.replaceAll("MODELNAME_TOKEN", modelName.toLowerCase());
+				String result = template.replaceAll("ENTITYNAME_TOKEN", entityMetaInfo.entityName);
+				result = result.replaceAll("MODELNAME_TOKEN", entityMetaInfo.modelName.toLowerCase());
 				return result;
 			}
 
@@ -244,14 +264,14 @@ public class CodeGenerator {
 			@Override
 			public String getTargetFilePath() {
 
-				return PROJECT_HOME + "src\\main\\webapp\\app\\litho-ui\\mydata\\resources\\" + entityName + "\\EntityEdit.tsx";
+				return PROJECT_HOME + "src\\main\\webapp\\app\\litho-ui\\mydata\\resources\\"+entityMetaInfo.modelName+"\\" + entityMetaInfo.entityName + "\\EntityEdit.tsx";
 
 			}
 
 			@Override
 			public String processTemplate(String template) {
-				String result = template.replaceAll("ENTITYNAME_TOKEN", entityName);
-				result = result.replaceAll("MODELNAME_TOKEN", modelName.toLowerCase());
+				String result = template.replaceAll("ENTITYNAME_TOKEN", entityMetaInfo.entityName);
+				result = result.replaceAll("MODELNAME_TOKEN", entityMetaInfo.modelName.toLowerCase());
 				return result;
 			}
 
@@ -261,14 +281,14 @@ public class CodeGenerator {
 			@Override
 			public String getTargetFilePath() {
 
-				return PROJECT_HOME + "src\\main\\webapp\\app\\litho-ui\\mydata\\resources\\" + entityName + "\\EntityList.tsx";
+				return PROJECT_HOME + "src\\main\\webapp\\app\\litho-ui\\mydata\\resources\\"+entityMetaInfo.modelName+"\\" + entityMetaInfo.entityName + "\\EntityList.tsx";
 
 			}
 
 			@Override
 			public String processTemplate(String template) {
-				String result = template.replaceAll("ENTITYNAME_TOKEN", entityName);
-				result = result.replaceAll("MODELNAME_TOKEN", modelName.toLowerCase());
+				String result = template.replaceAll("ENTITYNAME_TOKEN", entityMetaInfo.entityName);
+				result = result.replaceAll("MODELNAME_TOKEN", entityMetaInfo.modelName.toLowerCase());
 				return result;
 			}
 
@@ -278,14 +298,14 @@ public class CodeGenerator {
 			@Override
 			public String getTargetFilePath() {
 
-				return PROJECT_HOME + "src\\main\\webapp\\app\\litho-ui\\mydata\\resources\\" + entityName + "\\EntityPicker.tsx";
+				return PROJECT_HOME + "src\\main\\webapp\\app\\litho-ui\\mydata\\resources\\"+entityMetaInfo.modelName+"\\" + entityMetaInfo.entityName + "\\EntityPicker.tsx";
 
 			}
 
 			@Override
 			public String processTemplate(String template) {
-				String result = template.replaceAll("ENTITYNAME_TOKEN", entityName);
-				result = result.replaceAll("MODELNAME_TOKEN", modelName.toLowerCase());
+				String result = template.replaceAll("ENTITYNAME_TOKEN", entityMetaInfo.entityName);
+				result = result.replaceAll("MODELNAME_TOKEN", entityMetaInfo.modelName.toLowerCase());
 				return result;
 			}
 
@@ -295,14 +315,14 @@ public class CodeGenerator {
 			@Override
 			public String getTargetFilePath() {
 
-				return PROJECT_HOME + "src\\main\\webapp\\app\\litho-ui\\mydata\\resources\\" + entityName + "\\EntityResource.tsx";
+				return PROJECT_HOME + "src\\main\\webapp\\app\\litho-ui\\mydata\\resources\\"+entityMetaInfo.modelName+"\\" + entityMetaInfo.entityName + "\\EntityResource.tsx";
 
 			}
 
 			@Override
 			public String processTemplate(String template) {
-				String result = template.replaceAll("ENTITYNAME_TOKEN", entityName);
-				result = result.replaceAll("MODELNAME_TOKEN", modelName.toLowerCase());
+				String result = template.replaceAll("ENTITYNAME_TOKEN", entityMetaInfo.entityName);
+				result = result.replaceAll("MODELNAME_TOKEN", entityMetaInfo.modelName.toLowerCase());
 				return result;
 			}
 
@@ -313,7 +333,7 @@ public class CodeGenerator {
 			@Override
 			public String processFileText(String text) throws Exception {
 				{ // adding import
-					String element = "import { " + entityName + "RAResource } from 'app/litho-ui/mydata/resources/" + entityName + "/EntityResource';";
+					String element = "import { " + entityMetaInfo.entityName + "RAResource } from 'app/litho-ui/mydata/resources/"+entityMetaInfo.modelName+"/" + entityMetaInfo.entityName + "/EntityResource';";
 
 					if (!text.contains(element)) {
 						String replacement = //
@@ -324,7 +344,7 @@ public class CodeGenerator {
 					}
 				}
 				{ // adding resource
-					String element = "{" + entityName + "RAResource}";
+					String element = "{" + entityMetaInfo.entityName + "RAResource}";
 
 					if (!text.contains(element)) {
 						String replacement = //
--- utils/src/main/java/com/mtheile/utils/jhi/codegenerator/templates/ts/GetSubMenu.template.tsx
+++ utils/src/main/java/com/mtheile/utils/jhi/codegenerator/templates/ts/GetSubMenu.template.tsx
@@ -1,9 +1,6 @@
 import { MenuItemLink } from 'react-admin';
-import resources from 'app/litho-ui/mydata/resources/resources';
 import TocIcon from '@material-ui/icons/Toc';
 import * as React from 'react';
-// import { entityLIrradiationReactorResourceName } from 'app/litho-ui/mydata/resources/l-irradiation-reactor/EntityResource';
-// <!-- CODEGENERATOR_NEEDLE_FOR_ADDING_IMPORTS (don't remove) -->
 
 export function getSubMenuMODELNAME_TOKEN(onMenuClick: () => void, open: boolean, dense: boolean) {
   return (
